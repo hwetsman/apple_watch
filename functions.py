96175ea64b615df0d6585aba7349bba0d2e28eb3
hwetsman <howard@tocdr.com>
2022-08-06 08:53:42 -0500

    Code: create files list for subsets of data

--- functions.py
+++ functions.py
@@ -10,3 +10,21 @@ def Get_Data(path_to_watch_data):
     record_list = [x.attrib for x in root.iter('Record')]
     df = pd.DataFrame(record_list)
     return df
+
+
+files = ['DietarySugar', 'BodyMass', 'DietaryVitaminC',
+         'BloodGlucose', 'MindfulSession', 'AppleWalkingSteadiness',
+         'HeartRateVariabilitySDNN', 'HeartRate', 'RestingHeartRate',
+         'FlightsClimbed', 'DietarySodium', 'LeanBodyMass',
+         'WalkingSpeed', 'DietaryFatTotal', 'WalkingDoubleSupportPercentage',
+         'Height', 'DistanceWalkingRunning', 'VO2Max', 'AudioExposureEvent',
+         'WalkingAsymmetryPercentage', 'DietaryPotassium', 'BloodPressureSystolic',
+         'DietaryCarbohydrates', 'AppleExerciseTime', 'WalkingHeartRateAverage',
+         'AppleStandHour', 'BodyMassIndex', 'DietaryEnergyConsumed',
+         'DietaryCholesterol', 'EnvironmentalAudioExposure', 'SleepAnalysis',
+         'DietaryCalcium', 'AppleStandTime', 'DietaryFatPolyunsaturated',
+         'ActiveEnergyBurned', 'DietaryProtein', 'DietaryIron',
+         'BloodPressureDiastolic', 'DietaryFiber', 'RespiratoryRate',
+         'DietaryFatMonounsaturated', 'StepCount', 'SixMinuteWalkTestDistance',
+         'WalkingStepLength', 'BodyFatPercentage', 'BasalEnergyBurned',
+         'HeadphoneAudioExposureEvent', 'HeadphoneAudioExposure', 'DietaryFatSaturated']
